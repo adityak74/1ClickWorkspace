af32a251c7176bede0bc4fbb3d7d1e4554c5ed5f
Aditya K G <akarnam37@gmail.com>
2015-04-08 00:00:02 +0530

    FileHandler for Auto Listing

---
+++ fileHandler.py
@@ -0,0 +1,20 @@
+from Tkinter import *
+import os,sys
+
+def getDirectoryList(value=None):
+    if value is None:
+    	value = os.getcwd()
+    	path = value
+    	#print path
+    else:
+    	path = os.path.abspath(value)
+    	#print path
+    dirs = os.listdir(path)
+    #print dirs
+    for file in dirs:
+    	if(os.path.isdir(path + "/" + file)):
+    		print file
+    	
+
+#Testing the function by passing params
+#getDirectoryList("/opt/lampp/htdocs/")
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # write tkinter as Tkinter to be Python 2.x compatible
 from multi_os_base import *
+from fileHandler import *
 from Tkinter import *
+def hello():
+    tkMessageBox.showinfo("Say Hello", "Hello World")
 def getPath(event):
 	Lb1.insert(END,E1.get())
 
@@ -22,6 +25,30 @@ import tkMessageBox
 top = Tk()
 top.title("1Click-Workspace Manager");
 top.minsize(width=400,height=400)
+
+menubar = Menu(top)
+
+# create a pulldown menu, and add it to the menu bar
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Open", command=hello)
+filemenu.add_command(label="Save", command=hello)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=top.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+# create more pulldown menus
+editmenu = Menu(menubar, tearoff=0)
+editmenu.add_command(label="Cut", command=hello)
+editmenu.add_command(label="Copy", command=hello)
+editmenu.add_command(label="Paste", command=hello)
+menubar.add_cascade(label="Edit", menu=editmenu)
+
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="About", command=hello)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+# display the menu
+top.config(menu=menubar)
 
 var = StringVar()
 label = Label( top, text= 'Workspace Paths')
